abaff0f1bed50012ee05f16868ba475a2e75d2bf
Andr� Silveira <andresilveira_3101@hotmail.com>
2019-10-28 09:00:00 -0300

    test1

--- app/src/main/java/viana/andre/paroquiacm/ui/main/MainViewModel.kt
+++ app/src/main/java/viana/andre/paroquiacm/ui/main/MainViewModel.kt
@@ -3,5 +3,5 @@ package viana.andre.paroquiacm.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    // TODO: Implement the ViewMode
 }
